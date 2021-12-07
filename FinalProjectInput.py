0d11f4ebabc7aba3ebfc45cd9d4423e56d3f2363
philliphoangcoding <Mightyas123>
2021-12-06 21:15:26 -0600

    first commit

--- FinalProjectInput.py
+++ FinalProjectInput.py
@@ -8,24 +8,24 @@ from datetime import date
 
 #------------------ part a ------------------
 
-#  making some empty lists to save all the information from the csv files
+# making some empty lists to save all the information from the csv files
 manu_list = []
 price_list = []
 date_list = []
 # opening each csv file and appending them to the empty lists
 with open("ManufacturerList.csv") as manulist:
     manu_reader = csv.reader(manulist)
-    for line in manu_reader:
-        manu_list.append(line)
+    for x in manu_reader:
+        manu_list.append(x)
 with open("PriceList.csv") as pricelist:
     price_reader = csv.reader(pricelist)
-    for line in price_reader:
-        price_list.append(line)
+    for x in price_reader:
+        price_list.append(x)
 with open("ServiceDatesList.csv") as datelist:
     service_reader = csv.reader(datelist)
-    for line in service_reader:
-        date_list.append(line)
-# sorting each list alphabetically and saving the list as the sorted list
+    for x in service_reader:
+        date_list.append(x)
+# sorting each list by ID and saving the list as the sorted list
 manu_list = (sorted(manu_list, key=itemgetter(0)))
 price_list = (sorted(price_list, key=itemgetter(0)))
 date_list = (sorted(date_list, key=itemgetter(0)))
@@ -42,75 +42,68 @@ for x in range(0, len(final_list)):
 # removing the first damaged part of the list
 for x in range(0, len(final_list)):
     manu_list[x].pop(3)
-full_inventory = (sorted(final_list, key=itemgetter(1)))
-# writing the complete full inventory into FullInventory.csv
-with open('FullInventory.csv', 'w') as fullList:
-    fullwrite = csv.writer(fullList)
-    for x in range(0, len(full_inventory)):
-        fullwrite.writerow(full_inventory[x])
-
+full_inventory = (sorted(final_list, key=itemgetter(0)))
+#making a seperated list for the sorted full_inventory
+def Extract1(lst):
+    return [item[0] for item in lst]
+def Extract2(lst):
+    return [item[1] for item in lst]
+def Extract3(lst):
+    return [item[2] for item in lst]
+def Extract4(lst):
+    return [item[3] for item in lst]
+id_list = Extract1(full_inventory)
+manufacturer_list = Extract2(full_inventory)
+type_list = Extract3(full_inventory)
+cost_list = Extract4(full_inventory)
 #------------------ part b ------------------
-
-# making an empty list for each item type
-tower_list = []
-laptop_list = []
-phone_list = []
-# making an if statement to find the item types and appending them to the empty lists
-for x in range(0, len(final_list)):
-    if final_list[x][2] == "tower":
-        tower_list.append(final_list[x])
-    elif final_list[x][2] == "phone":
-        phone_list.append(final_list[x])
+while True:
+    #prompt the user for the query
+    user = input("Type a query or q to quit: ")
+    #if the user enters q exit the program
+    if(user == "q"):
+        break
+    user = user.split()
+    manu = ""
+    type = ""
+    for word in user:
+        #iterate over manu list
+        for i in manufacturer_list:
+            if str(i).lower() == str(word).lower():
+                #assign the manufacturer to item
+                manu = i
+        #iterate over type list
+        for i in type_list:
+            if str(i).lower() == str(word).lower():
+                #assign the type
+                type = i
+    #if either item or type is empty print it
+    if(manu == "" or type == ""):
+        print("No such item in inventory")
     else:
-        laptop_list.append(final_list[x])
-# sorting each list by ID (assumed least to greatest)
-tower_list = (sorted(tower_list, key=itemgetter(0)))
-laptop_list = (sorted(laptop_list, key=itemgetter(0)))
-phone_list = (sorted(phone_list, key=itemgetter(0)))
-# writing a file for each item type
-with open('LaptopInventory.csv', 'w') as laptopList:
-    laptop_write = csv.writer(laptopList)
-    for x in range(0, len(laptop_list)):
-        laptop_write.writerow(laptop_list[x])
-with open('PhoneInventory.csv', 'w') as phoneList:
-    phone_write = csv.writer(phoneList)
-    for x in range(0, len(phone_list)):
-        phone_write.writerow(phone_list[x])
-with open('TowerInventory.csv', 'w') as towerList:
-    tower_write = csv.writer(towerList)
-    for x in range(0, len(tower_list)):
-        tower_write.writerow(tower_list[x])
-
-#------------------ part c ------------------
-
-# used this function to get the present date and put the format to the same as the csv files
-today = date.today().strftime('%d/%m/%Y')
-# making an empty list for the dates
-pastdate = []
-# went through the service dates column to find if today's date is greater than it
-for x in range(0, len(final_list)):
-    if final_list[x][4] < today:
-        pastdate.append(item_type[x])
-# sorting the file by date
-pastdate = (sorted(pastdate, key=itemgetter(-1), reverse=True))
-# writing each past service date to PastServiceDateInventory.csv
-with open('PastServiceDateInventory.csv', 'w') as date_List:
-    pswrite = csv.writer(date_List)
-    for x in range(0, len(pastdate)):
-        pswrite.writerow(pastdate[x])
-
-#------------------ part d ------------------
-
-# made an empty list to hold all damaged items
-damagedlist = []
-# went through the damaged indicator column to find if it's damaged and appended it
-for x in range(0, len(final_list)):
-    if final_list[x][5] == "damaged":
-        damagedlist.append(final_list[x])
-# sorted the damaged list by most expensive to least
-damagedlist = (sorted(damagedlist, key=itemgetter(4), reverse=True))
-# writing a damaged products to DamagedInventory.csv
-with open('DamagedInventory.csv', 'w') as damaged_List:
-    diwrite = csv.writer(damaged_List)
-    for x in range(0, len(damagedlist)):
-        diwrite.writerow(damagedlist[x])
+        #initialize the list to store the values
+        temp_list_final = []
+        #iterate over amount of data we have
+        for i in range(len(id_list)):
+            #check the data of manufacturer and type in the data
+            if(manufacturer_list[i] == manu and type_list[i] == type):
+                temp_list = []
+                temp_list.append(cost_list[i])
+                temp_list.append(id_list[i])
+                temp_list.append(manufacturer_list[i])
+                temp_list.append(type_list[i])
+                temp_list_final.append(temp_list)
+        #sort the list
+        output = (sorted( temp_list_final,key=itemgetter(0),reverse=True))
+        print("Your item is " + str(output[0][1]) + " " + str(output[0][2]) + " " + str(output[0][3]) + " " + str(output[0][0]))
+        #create a list to store the recommended items
+        extra = ["", "", "", 0]
+        #iterate over the amount of data we have
+        for i in range(len(id_list)):
+            #check all the items of same type and different manufacturer
+            if(type_list[i] == type and manufacturer_list[i] != manu):
+                extra[0] = id_list[i]
+                extra[1] = manufacturer_list[i]
+                extra[2] = type_list[i]
+                extra[3] = cost_list[i]
+                print("You may also like " + str(extra[0]) + " " + str(extra[1]) + " " + str(extra[2]) + " " + str(extra[3]))
